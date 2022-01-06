ae81b1fd4e539671abf974e39825cd1c070ea116
cristian <cris300oso@gmail.com>
2022-01-06 23:23:40 +0100

    put delete listos

--- src/main/java/com/courseSpringBoot/domain/service/StudentService.java
+++ src/main/java/com/courseSpringBoot/domain/service/StudentService.java
@@ -4,10 +4,12 @@ import com.courseSpringBoot.domain.repository.StudentRepository;
 import com.courseSpringBoot.persistence.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -32,5 +34,33 @@ public class StudentService {
             throw new IllegalStateException("email take");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exist = studentRepository.existsById(studentId);
+        if(!exist){
+            throw new IllegalStateException("student with id "+studentId+" does not exits");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional//con esta anotacion no es necesario llamar al repository
+    public void updateStudent(String name, String email,Long id) {
+        boolean existStudent = studentRepository.existsById(id);
+        if(!existStudent){
+            throw new IllegalStateException("the student no exist");
+        }
+
+        Student student = studentRepository.getById(id);
+        if(name!=null && name.length()>0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email!=null && email.length()>0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email take");
+            }
+            student.setEmail(email);
+        }
     }
 }
--- src/main/java/com/courseSpringBoot/web/controller/StudentController.java
+++ src/main/java/com/courseSpringBoot/web/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.courseSpringBoot.web.controller;
 import com.courseSpringBoot.domain.service.StudentService;
 import com.courseSpringBoot.persistence.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,5 +28,14 @@ public class StudentController {
     @PostMapping("/postStudent")
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
+    }
+    @DeleteMapping("/deleteStudent/{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping("/putStudent/{studentId}")
+    public void updateStudent(@RequestParam(required = false) String name,@RequestParam(required = false) String email,@PathVariable("studentId") Long id){
+        studentService.updateStudent(name,email,id);
     }
 }
